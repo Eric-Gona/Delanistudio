53f28d0b644ddfc2a0bd74ea3a7ddf901c0adc66
Eric-Gona <eric.gona254@gmail.com>
2021-11-20 08:58:48 +0300

    adds js code

---
+++ script.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+
+jQuery(window).load(function() {
+var container = document.querySelector('#container');
+var msnry = new Masonry( container, {
+itemSelector: '.masonryImage';
+columnWidth: '.masonryImage';              
+}); 
+
+});
+</script>
