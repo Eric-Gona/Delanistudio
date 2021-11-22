2b45797c1d6724e77fe527abd1b70af45510bc26
Eric-Gona <eric.gona254@gmail.com>
2021-11-22 10:08:33 +0300

    adds code

--- script.js
+++ script.js
@@ -26,79 +26,80 @@
               $("#img3").toggle();
                 $("#product").toggle();
               });
-// PORTFOLIO BEGINS
-$("#pc1").mouseenter(function(){
-  $(".txt1").show(2000);
-  
-});
-$("#pc1").mouseleave(function(){
- $(".txt1").hide(1000);
-});
-
-
-$("#pc2").mouseenter(function(){
-  $(".txt2").show(2000);
-  
-});
-$("#pc2").mouseleave(function(){
- $(".txt2").hide(1000);
-});
-
-
-$("#pc3").mouseenter(function(){
-  $(".txt3").show(2000);
-  
-});
-$("#pc3").mouseleave(function(){
- $(".txt3").hide(1000);
-});
-
-
-$("#pc4").mouseenter(function(){
-  $(".txt4").show(2000);
-  
-});
-$("#pc4").mouseleave(function(){
- $(".txt4").hide(1000);
-});
-
-
-
-$("#pc5").mouseenter(function(){
-  $(".txt5").show(2000);
-  
-});
-$("#pc5").mouseleave(function(){
- $(".txt5").hide(1000);
-});
-
-
-
-$("#pc6").mouseenter(function(){
-  $(".txt6").show(2000);
-  
-});
-$("#pc6").mouseleave(function(){
- $(".txt6").hide(1000);
-});
-$("#pc6").mouseenter(function(){
-  $(".txt6").show(2000);
-  
-});
-$("#pc7").mouseleave(function(){
- $(".txt7").hide(1000);
-});
-$("#pc7").mouseenter(function(){
-  $(".txt7").show(2000);  
-});
-
-$("#pc8").mouseleave(function(){
- $(".txt8").hide(1000);
-});
-$("#pc8").mouseenter(function(){
-  $(".txt8").show(2000); 
-}); 
-                function submit(fo){
+ // PORTFOLIO BEGINS
+       $("#pc1").mouseenter(function(){
+        $(".txt1").show(2000);
+        
+      });
+      $("#pc1").mouseleave(function(){
+       $(".txt1").hide(1000);
+      });
+      
+      
+      $("#pc2").mouseenter(function(){
+        $(".txt2").show(2000);
+        
+      });
+      $("#pc2").mouseleave(function(){
+       $(".txt2").hide(1000);
+      });
+      
+      
+      $("#pc3").mouseenter(function(){
+        $(".txt3").show(2000);
+        
+      });
+      $("#pc3").mouseleave(function(){
+       $(".txt3").hide(1000);
+      });
+      
+      
+      $("#pc4").mouseenter(function(){
+        $(".txt4").show(2000);
+        
+      });
+      $("#pc4").mouseleave(function(){
+       $(".txt4").hide(1000);
+      });
+      
+      
+      
+      $("#pc5").mouseenter(function(){
+        $(".txt5").show(2000);
+        
+      });
+      $("#pc5").mouseleave(function(){
+       $(".txt5").hide(1000);
+      });
+      
+      
+      
+      $("#pc6").mouseenter(function(){
+        $(".txt6").show(2000);
+        
+      });
+      $("#pc6").mouseleave(function(){
+       $(".txt6").hide(1000);
+      });
+      $("#pc6").mouseenter(function(){
+        $(".txt6").show(2000);
+        
+      });
+      $("#pc7").mouseleave(function(){
+       $(".txt7").hide(1000);
+      });
+      $("#pc7").mouseenter(function(){
+        $(".txt7").show(2000);  
+      });
+      
+      $("#pc8").mouseleave(function(){
+       $(".txt8").hide(1000);
+      });
+      $("#pc8").mouseenter(function(){
+        $(".txt8").show(2000); 
+      }); 
+                  
+    function submit(fo){
                   var name= document.getElementById("name").value;
                   var email = document.getElementById("email").value;
                   alert("submmission received" + name);
